21607ed62328c4f74e20fad3a623d4c3e338a7ce
carolhybank <caroline.silva@hybank.biz>
2020-10-17 01:07:40 -0300

    Commit initial

---
+++ src/components/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Home() {
+return(
+    <div>
+        
+        <div>
+            <h2>Pergunta aqui</h2>
+            <button>Ver resposta</button>
+            <p>Resposta</p>
+        </div>
+    </div>
+)
+
+
+}
+    
---
+++ src/data.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        questions: 'O condutor bl bla bla ',
+        awsner: 'resposta blabla bla',
+    },
+    {
+        questions: 'O condutor bl bla bla ',
+        awsner: 'resposta blabla bla',
+    },
+    {
+        questions: 'O condutor bl bla bla ',
+        awsner: 'resposta blabla bla',
+    }
+]
